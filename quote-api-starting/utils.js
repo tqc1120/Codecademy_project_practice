f5900b1191facf4607519b8fe1481a01c5d0c58f
oreo <tqc1120@gmail.com>
2021-06-18 15:54:18 -0400

    get quote randomly/by name

--- quote-api-starting/utils.js
+++ quote-api-starting/utils.js
@@ -1,8 +1,12 @@
 const getRandomElement = arr => {
   if (!Array.isArray(arr)) throw new Error('Expected an array');
   return arr[Math.floor(Math.random() * arr.length)];
-}
-
-module.exports = {
-  getRandomElement
 };
+
+const getIndexById = (id, arr) => {
+  return arr.findIndex((quote) => {
+    return quote.id == id;
+  });
+};
+
+module.exports = { getRandomElement, getIndexById };

8617fc43141a09c7b6ff8fe379badb12835f4fc5
oreo <tqc1120@gmail.com>
2021-06-30 20:37:33 -0400

    delete useless

--- Matching_Momory/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers, createStore } from 'redux';
-
-import { boardReducer } from '../features/board/boardSlice.js';
-
-const rootReducer = combineReducers({
-  board: boardReducer,
-});
-
-export const store = createStore(rootReducer);
